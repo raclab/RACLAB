7bca702ad727ef145c0ff3633741d3d2fe1d5baa
Muhammed Shames <33106854+MuhammedShames@users.noreply.github.com>
2018-05-19 14:27:12 +0300

    Update Arduino_rasp_haberleşme.py

--- Lisans Projeleri/Image Processing Based Search & Rescue Robot/Arduino_rasp_haberleşme.py
+++ Lisans Projeleri/Image Processing Based Search & Rescue Robot/Arduino_rasp_haberleşme.py
@@ -2,5 +2,5 @@ import serial
 import time
 ser = serial.Serial('/dev/ttyUSB0', 9600) #Burada ttyUSB0 arduino kablosunun bagladigimiz raspberry portun adidir, kullanilan USB portun adi ogrenilip degistirmek gerekebilir.
 
-ser.write('aruino koduna belirlenen bir degisken') #ornegin ser.write('1') 
+ser.write('aruino kodunda belirlenen bir degisken') #ornegin ser.write('1') 
 
